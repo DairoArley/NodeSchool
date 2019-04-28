6172aeb2b29c0a55ae329014c67bbf914c994cfa
DairoArley <dagavergara@gmail.com>
2019-04-28 12:42:28 -0500

    Second commit

--- learnyounode/program.js
+++ learnyounode/program.js
@@ -5,7 +5,5 @@ for (var i = 2; i<array.lenth; i++){
     console.log(process.argv[i]);
     sum = Number(process.argv[i]) + sum;
 }
-/*process.argv.forEach(element => {
-    sum = Number (element + sum);
-});*/
+
 console.log(sum);
